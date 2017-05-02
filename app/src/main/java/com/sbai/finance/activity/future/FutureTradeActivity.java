5bed439507e7afb8dcab25c4649af0382c703fe2
John Zhang <zxukai@gmail.com>
2017-05-02 11:28:19 +0800

    添加期货交易页面的详情显示

--- app/src/main/java/com/sbai/finance/activity/future/FutureTradeActivity.java
+++ app/src/main/java/com/sbai/finance/activity/future/FutureTradeActivity.java
@@ -24,29 +24,34 @@ import com.sbai.finance.activity.BaseActivity;
 import com.sbai.finance.fragment.PredictionFragment;
 import com.sbai.finance.fragment.trade.IntroduceFragment;
 import com.sbai.finance.fragment.trade.OpinionFragment;
+import com.sbai.finance.model.FutureData;
 import com.sbai.finance.model.Variety;
 import com.sbai.finance.net.Callback2D;
 import com.sbai.finance.net.Client;
 import com.sbai.finance.net.Resp;
+import com.sbai.finance.netty.Netty;
+import com.sbai.finance.netty.NettyHandler;
 import com.sbai.finance.utils.Display;
+import com.sbai.finance.utils.FinanceUtil;
 import com.sbai.finance.utils.Launcher;
 import com.sbai.finance.view.TitleBar;
 import com.sbai.finance.view.TradeFloatButtons;
 import com.sbai.finance.view.slidingTab.SlidingTabLayout;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import static com.sbai.finance.R.id.klineView;
-
 public class FutureTradeActivity extends BaseActivity {
 
     @BindView(R.id.titleBar)
     TitleBar mTitleBar;
 
+    @BindView(R.id.priceChange)
+    TextView mPriceChange;
     @BindView(R.id.todayOpen)
     TextView mTodayOpen;
     @BindView(R.id.preClose)
@@ -60,7 +65,7 @@ public class FutureTradeActivity extends BaseActivity {
     TabLayout mTabLayout;
     @BindView(R.id.trendView)
     TrendView mTrendView;
-    @BindView(klineView)
+    @BindView(R.id.klineView)
     KlineView mKlineView;
 
     @BindView(R.id.tradeFloatButtons)
@@ -75,6 +80,8 @@ public class FutureTradeActivity extends BaseActivity {
     LinearLayout mChartArea;
     @BindView(R.id.subPageArea)
     LinearLayout mSubPageArea;
+    @BindView(R.id.lastPrice)
+    TextView mLastPrice;
 
     private SubPageAdapter mSubPageAdapter;
     private Variety mVariety;
@@ -97,6 +104,20 @@ public class FutureTradeActivity extends BaseActivity {
         initFloatBar();
     }
 
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Netty.get().subscribe(Netty.REQ_SUB, mVariety.getContractsCode());
+        Netty.get().addHandler(mNettyHandler);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Netty.get().subscribe(Netty.REQ_UNSUB, mVariety.getContractsCode());
+        Netty.get().removeHandler(mNettyHandler);
+    }
+
     private void initData() {
         mVariety = getIntent().getParcelableExtra(Launcher.EX_PAYLOAD);
     }
@@ -113,7 +134,7 @@ public class FutureTradeActivity extends BaseActivity {
     private void initChartViews() {
         TrendView.Settings settings = new TrendView.Settings();
         settings.setBaseLines(mVariety.getBaseline());
-        settings.setNumberScale(mVariety.getPriceDecimalScale());
+        settings.setNumberScale(mVariety.getPriceScale());
         settings.setOpenMarketTimes(mVariety.getOpenMarketTime());
         settings.setDisplayMarketTimes(mVariety.getDisplayMarketTimes());
         settings.setLimitUpPercent((float) mVariety.getLimitUpPercent());
@@ -122,7 +143,7 @@ public class FutureTradeActivity extends BaseActivity {
 
         KlineChart.Settings settings2 = new KlineChart.Settings();
         settings2.setBaseLines(mVariety.getBaseline());
-        settings2.setNumberScale(mVariety.getPriceDecimalScale());
+        settings2.setNumberScale(mVariety.getPriceScale());
         settings2.setXAxis(40);
         settings2.setIndexesType(KlineChart.Settings.INDEXES_VOL);
         mKlineView.setSettings(settings2);
@@ -313,4 +334,36 @@ public class FutureTradeActivity extends BaseActivity {
                 }).fire();
     }
 
+    private NettyHandler mNettyHandler = new NettyHandler<Resp<FutureData>>() {
+        @Override
+        public void onReceiveData(Resp<FutureData> data) {
+            if (data.getCode() == Netty.REQ_QUOTA) {
+                updateMarketDataView(data.getData());
+            }
+        }
+    };
+
+    private void updateMarketDataView(FutureData data) {
+        mLastPrice.setText(FinanceUtil.formatWithScale(data.getLastPrice(), mVariety.getPriceScale()));
+        double priceChange = FinanceUtil.subtraction(data.getLastPrice(), data.getPreSetPrice()).doubleValue();
+        double priceChangePercent = FinanceUtil.divide(priceChange, data.getPreSetPrice(), 4)
+                .multiply(new BigDecimal(100)).doubleValue();
+
+        mLastPrice.setTextColor(ContextCompat.getColor(getActivity(), R.color.greenAssist));
+        mPriceChange.setTextColor(ContextCompat.getColor(getActivity(), R.color.greenAssist));
+        String priceChangeStr = FinanceUtil.formatWithScale(priceChange, mVariety.getPriceScale());
+        String priceChangePercentStr = FinanceUtil.formatWithScale(priceChangePercent) + "%";
+        if (priceChange >= 0) {
+            priceChangeStr = "+" + priceChangeStr;
+            priceChangePercentStr = "+" + priceChangePercentStr;
+            mLastPrice.setTextColor(ContextCompat.getColor(getActivity(), R.color.redPrimary));
+            mPriceChange.setTextColor(ContextCompat.getColor(getActivity(), R.color.redPrimary));
+        }
+        mPriceChange.setText(priceChangeStr + "     " + priceChangePercentStr);
+
+        mTodayOpen.setText(FinanceUtil.formatWithScale(data.getOpenPrice(), mVariety.getPriceScale()));
+        mHighest.setText(FinanceUtil.formatWithScale(data.getHighestPrice(), mVariety.getPriceScale()));
+        mLowest.setText(FinanceUtil.formatWithScale(data.getLowestPrice(), mVariety.getPriceScale()));
+        mPreClose.setText(FinanceUtil.formatWithScale(data.getPreClsPrice(), mVariety.getPriceScale()));
+    }
 }
--- app/src/main/java/com/sbai/finance/fragment/future/FutureFragment.java
+++ app/src/main/java/com/sbai/finance/fragment/future/FutureFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +17,19 @@ import android.widget.TextView;
 import com.sbai.finance.R;
 import com.sbai.finance.activity.future.FutureTradeActivity;
 import com.sbai.finance.fragment.BaseFragment;
+import com.sbai.finance.model.FutureData;
+import com.sbai.finance.model.Variety;
 import com.sbai.finance.net.Callback2D;
 import com.sbai.finance.net.Client;
 import com.sbai.finance.net.Resp;
-import com.sbai.finance.model.Variety;
+import com.sbai.finance.netty.Netty;
+import com.sbai.finance.netty.NettyHandler;
+import com.sbai.finance.utils.FinanceUtil;
 import com.sbai.finance.utils.Launcher;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.List;
 
 import butterknife.BindView;
@@ -44,9 +51,10 @@ public class FutureFragment extends BaseFragment {
 
     private FutureListAdapter mFutureListAdapter;
 
-    private String mfutureType;
+    private String mFutureType;
     private Integer mPage = 0;
     private Integer mPageSize = 15;
+
     public static FutureFragment newInstance(String type) {
         FutureFragment futureFragment = new FutureFragment();
         Bundle bundle = new Bundle();
@@ -59,9 +67,10 @@ public class FutureFragment extends BaseFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mfutureType = getArguments().getString("type");
+            mFutureType = getArguments().getString("type");
         }
     }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -99,7 +108,13 @@ public class FutureFragment extends BaseFragment {
     public void onResume() {
         super.onResume();
         requestVarietyList();
+        Netty.get().addHandler(mNettyHandler);
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        Netty.get().removeHandler(mNettyHandler);
     }
 
     @OnClick(R.id.rate)
@@ -107,8 +122,49 @@ public class FutureFragment extends BaseFragment {
 
     }
 
+    private NettyHandler mNettyHandler = new NettyHandler<Resp<FutureData>>() {
+        @Override
+        public void onReceiveData(Resp<FutureData> data) {
+            if (data.getCode() == Netty.REQ_QUOTA) {
+                updateListViewVisibleItem(data.getData());
+                mFutureListAdapter.addFutureData(data.getData());
+            }
+        }
+    };
+
+    private void updateListViewVisibleItem(FutureData data) {
+        if (mListView != null && mFutureListAdapter != null) {
+            int first = mListView.getFirstVisiblePosition();
+            int last = mListView.getLastVisiblePosition();
+            for (int i = first; i <= last; i++) {
+                Variety variety = mFutureListAdapter.getItem(i);
+                if (variety != null
+                        && data.getInstrumentId().equalsIgnoreCase(variety.getContractsCode())) {
+                    View childView = mListView.getChildAt(i - mListView.getFirstVisiblePosition());
+                    if (childView != null) {
+                        TextView lastPrice = ButterKnife.findById(childView, R.id.lastPrice);
+                        TextView rate = ButterKnife.findById(childView, R.id.rate);
+                        double priceChange = FinanceUtil.subtraction(data.getLastPrice(), data.getPreSetPrice())
+                                .divide(new BigDecimal(data.getPreSetPrice()), 4, RoundingMode.HALF_EVEN)
+                                .multiply(new BigDecimal(100)).doubleValue();
+                        lastPrice.setText(FinanceUtil.formatWithScale(data.getLastPrice(), variety.getPriceScale()));
+                        if (priceChange >= 0) {
+                            lastPrice.setTextColor(ContextCompat.getColor(getActivity(), R.color.redPrimary));
+                            rate.setTextColor(ContextCompat.getColor(getActivity(), R.color.redPrimary));
+                            rate.setText("+" + FinanceUtil.formatWithScale(priceChange) + "%");
+                        } else {
+                            lastPrice.setTextColor(ContextCompat.getColor(getActivity(), R.color.greenAssist));
+                            rate.setTextColor(ContextCompat.getColor(getActivity(), R.color.greenAssist));
+                            rate.setText("-" + FinanceUtil.formatWithScale(priceChange) + "%");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private void updateFutureData(List<Variety> varietyList) {
-        if (varietyList == null){
+        if (varietyList == null) {
             return;
         }
         mFutureListAdapter.clear();
@@ -123,7 +179,7 @@ public class FutureFragment extends BaseFragment {
     }
 
     public void requestVarietyList() {
-        Client.getVarietyList(Variety.VAR_FUTURE, mPage, mPageSize, mfutureType)
+        Client.getVarietyList(Variety.VAR_FUTURE, mPage, mPageSize, mFutureType)
                 .setTag(TAG).setIndeterminate(this)
                 .setCallback(new Callback2D<Resp<List<Variety>>, List<Variety>>() {
                     @Override
@@ -134,9 +190,16 @@ public class FutureFragment extends BaseFragment {
     }
 
     public static class FutureListAdapter extends ArrayAdapter<Variety> {
-        Context mContext;
+
+        private HashMap<String, FutureData> mFutureDataList;
+
         public FutureListAdapter(@NonNull Context context) {
             super(context, 0);
+            mFutureDataList = new HashMap<>();
+        }
+
+        public void addFutureData(FutureData futureData) {
+            mFutureDataList.put(futureData.getInstrumentId(), futureData);
         }
 
         @NonNull
@@ -150,11 +213,12 @@ public class FutureFragment extends BaseFragment {
             } else {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
-            viewHolder.bindDataWithView(getItem(position), position, getContext());
+            viewHolder.bindingData(getItem(position), mFutureDataList, getContext());
             return convertView;
         }
 
         static class ViewHolder {
+
             @BindView(R.id.futureName)
             TextView mFutureName;
             @BindView(R.id.futureCode)
@@ -171,12 +235,31 @@ public class FutureFragment extends BaseFragment {
             ViewHolder(View view) {
                 ButterKnife.bind(this, view);
             }
-            private void bindDataWithView(Variety item, int position, Context context) {
+
+            private void bindingData(Variety item, HashMap<String, FutureData> map, Context context) {
                 mFutureName.setText(item.getVarietyName());
                 mFutureCode.setText(item.getContractsCode());
-                mLastPrice.setText(item.getBigVarietyTypeCode());
-                mLastPrice.setText("66.66");
-                mRate.setText("+" + "0.00%");
+
+                FutureData futureData = map.get(item.getContractsCode());
+                if (futureData != null) {
+                    double priceChange = FinanceUtil.subtraction(futureData.getLastPrice(), futureData.getPreSetPrice())
+                            .divide(new BigDecimal(futureData.getPreSetPrice()), 4, RoundingMode.HALF_EVEN)
+                            .multiply(new BigDecimal(100)).doubleValue();
+                    mLastPrice.setText(FinanceUtil.formatWithScale(futureData.getLastPrice(), item.getPriceScale()));
+                    if (priceChange >= 0) {
+                        mLastPrice.setTextColor(ContextCompat.getColor(context, R.color.redPrimary));
+                        mRate.setTextColor(ContextCompat.getColor(context, R.color.redPrimary));
+                        mRate.setText("+" + FinanceUtil.formatWithScale(priceChange) + "%");
+                    } else {
+                        mLastPrice.setTextColor(ContextCompat.getColor(context, R.color.greenAssist));
+                        mRate.setTextColor(ContextCompat.getColor(context, R.color.greenAssist));
+                        mRate.setText("-" + FinanceUtil.formatWithScale(priceChange) + "%");
+                    }
+                } else {
+                    mLastPrice.setText("--");
+                    mRate.setText("--.--%");
+                }
+
                 if (item.getExchangeStatus() == Variety.EXCHANGE_STATUS_CLOSE) {
                     mTrade.setVisibility(View.GONE);
                     mStopTrade.setVisibility(View.VISIBLE);
