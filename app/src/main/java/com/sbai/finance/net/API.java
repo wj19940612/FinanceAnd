78089668ca7a9d3e1f5fe9da67bf267c020ad6aa
John Zhang <zxukai@gmail.com>
2017-05-05 12:05:09 +0800

    修改测试环境域名

--- app/src/main/java/com/sbai/finance/net/API.java
+++ app/src/main/java/com/sbai/finance/net/API.java
@@ -22,7 +22,7 @@ import java.util.Set;
 public class API extends RequestManager {
 
     // private final static String HOST = "http://forex2.esongbai.xyz";
-    private final static String HOST = "http://var.esongbai.xyz";
+    private final static String HOST = "http://fanli.esongbai.xyz";
     private static String mHost;
 
     private static Set<String> sCurrentUrls = new HashSet<>();
