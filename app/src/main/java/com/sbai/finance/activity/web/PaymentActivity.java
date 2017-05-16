6e94f0ebbf0ce98487dad0679d04d5a48c3c484c
licunkuan <licunkuan@hashxy.com>
2017-05-16 20:14:35 +0800

    修复禅道bug,新增支付功能

---
+++ app/src/main/java/com/sbai/finance/activity/web/PaymentActivity.java
@@ -0,0 +1,13 @@
+package com.sbai.finance.activity.web;
+
+import android.os.Bundle;
+
+import com.sbai.finance.activity.WebActivity;
+
+public class PaymentActivity extends WebActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
