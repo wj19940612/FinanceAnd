34043bb760ecca4da7d8d23eddc4b6222dce9591
王杰 <wangjie@hashxy.com>
2017-05-17 10:22:00 +0800

    增加股票涨幅榜接口

--- app/src/main/java/com/sbai/finance/fragment/stock/PriceLimitRankingFragment.java
+++ app/src/main/java/com/sbai/finance/fragment/stock/PriceLimitRankingFragment.java
@@ -1,28 +1,47 @@
 package com.sbai.finance.fragment.stock;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.sbai.finance.R;
+import com.sbai.finance.model.stock.StockData;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 
 public class PriceLimitRankingFragment extends Fragment {
 
+    private static final String KEY_SORT_TYPE = "sort_type";
+    private static final String KEY_STOCK_TYPE = "sort_type";
+
+    @BindView(android.R.id.list)
+    ListView mListView;
+    @BindView(android.R.id.empty)
+    AppCompatTextView mEmpty;
 
     private Unbinder mBind;
+    private int mSortType;
+    private int mStockType;
 
     public PriceLimitRankingFragment() {
     }
 
-    public static PriceLimitRankingFragment newInstance(String param1, String param2) {
+    public static PriceLimitRankingFragment newInstance(int sort_type, int stock_type) {
         PriceLimitRankingFragment fragment = new PriceLimitRankingFragment();
         Bundle args = new Bundle();
+        args.putInt(KEY_SORT_TYPE, sort_type);
+        args.putInt(KEY_STOCK_TYPE, stock_type);
         fragment.setArguments(args);
         return fragment;
     }
@@ -31,7 +50,8 @@ public class PriceLimitRankingFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-
+            mSortType = getArguments().getInt(KEY_SORT_TYPE);
+            mStockType = getArguments().getInt(KEY_STOCK_TYPE);
         }
     }
 
@@ -44,8 +64,32 @@ public class PriceLimitRankingFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mListView.setEmptyView(mEmpty);
+
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         mBind.unbind();
+    }
+
+    class StockSortAdapter extends ArrayAdapter<StockData> {
+
+        private Context mContext;
+
+        public StockSortAdapter(@NonNull Context context) {
+            super(context, 0);
+            mContext = context;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            return super.getView(position, convertView, parent);
+        }
     }
 }
