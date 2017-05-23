7b46165ebc6924bc5bf75fa806e8f5a60a98fbcf
licunkuan <licunkuan@hashxy.com>
2017-05-23 11:07:32 +0800

    修复借款有道view复用的问题

--- app/src/main/java/com/sbai/finance/activity/economiccircle/BorrowMoneyDetailsActivity.java
+++ app/src/main/java/com/sbai/finance/activity/economiccircle/BorrowMoneyDetailsActivity.java
@@ -307,24 +307,17 @@ public class BorrowMoneyDetailsActivity extends BaseActivity {
 			}
 		});
 
-		if (borrowMoneyDetails.getContentImg() != null) {
+		if (!TextUtils.isEmpty(borrowMoneyDetails.getContentImg())) {
 			String[] images = borrowMoneyDetails.getContentImg().split(",");
 			switch (images.length) {
 				case 1:
-					if (TextUtils.isEmpty(images[0])) {
-						mImage1.setVisibility(View.GONE);
-						mImage2.setVisibility(View.GONE);
-						mImage3.setVisibility(View.GONE);
-						mImage4.setVisibility(View.GONE);
-					} else {
-						mContentImg.setVisibility(View.VISIBLE);
-						mImage1.setVisibility(View.VISIBLE);
-						loadImage(context, images[0], mImage1);
-						mImage2.setVisibility(View.INVISIBLE);
-						mImage3.setVisibility(View.INVISIBLE);
-						mImage4.setVisibility(View.INVISIBLE);
-						imageClick(context, images, mImage1, 0);
-					}
+					mContentImg.setVisibility(View.VISIBLE);
+					mImage1.setVisibility(View.VISIBLE);
+					loadImage(context, images[0], mImage1);
+					mImage2.setVisibility(View.INVISIBLE);
+					mImage3.setVisibility(View.INVISIBLE);
+					mImage4.setVisibility(View.INVISIBLE);
+					imageClick(context, images, mImage1, 0);
 					break;
 				case 2:
 					mContentImg.setVisibility(View.VISIBLE);
--- app/src/main/java/com/sbai/finance/fragment/EconomicCircleFragment.java
+++ app/src/main/java/com/sbai/finance/fragment/EconomicCircleFragment.java
@@ -224,7 +224,7 @@ public class EconomicCircleFragment extends BaseFragment implements AbsListView.
 					requestEconomicCircleList();
 				}
 			});
-			mListView.addFooterView(mFootView, null,true);
+			mListView.addFooterView(mFootView, null, true);
 		}
 
 		if (economicCircleList.size() < mPageSize) {
@@ -544,24 +544,17 @@ public class EconomicCircleFragment extends BaseFragment implements AbsListView.
 					}
 				});
 
-				if (item.getContentImg() != null) {
+				if (!TextUtils.isEmpty(item.getContentImg())) {
 					String[] images = item.getContentImg().split(",");
 					switch (images.length) {
 						case 1:
-							if (TextUtils.isEmpty(images[0])) {
-								mImage1.setVisibility(View.GONE);
-								mImage2.setVisibility(View.GONE);
-								mImage3.setVisibility(View.GONE);
-								mImage4.setVisibility(View.GONE);
-							} else {
-								mContentImg.setVisibility(View.VISIBLE);
-								mImage1.setVisibility(View.VISIBLE);
-								loadImage(context, images[0], mImage1);
-								mImage2.setVisibility(View.INVISIBLE);
-								mImage3.setVisibility(View.INVISIBLE);
-								mImage4.setVisibility(View.INVISIBLE);
-								imageClick(context, images, mImage1, 0);
-							}
+							mContentImg.setVisibility(View.VISIBLE);
+							mImage1.setVisibility(View.VISIBLE);
+							loadImage(context, images[0], mImage1);
+							mImage2.setVisibility(View.INVISIBLE);
+							mImage3.setVisibility(View.INVISIBLE);
+							mImage4.setVisibility(View.INVISIBLE);
+							imageClick(context, images, mImage1, 0);
 							break;
 						case 2:
 							mContentImg.setVisibility(View.VISIBLE);
