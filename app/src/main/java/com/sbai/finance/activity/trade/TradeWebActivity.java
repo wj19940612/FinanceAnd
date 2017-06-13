17c64b94386f34d4a25b6cb27e993482ed0d18d8
林荣芳 <linrongfang@easybaocn.com>
2017-06-13 18:38:44 +0800

    修改原油期货宝页面标题

--- app/src/main/java/com/sbai/finance/activity/trade/TradeWebActivity.java
+++ app/src/main/java/com/sbai/finance/activity/trade/TradeWebActivity.java
@@ -3,6 +3,7 @@ package com.sbai.finance.activity.trade;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.sbai.finance.R;
 import com.sbai.finance.activity.WebActivity;
 
 /**
@@ -16,12 +17,13 @@ public class TradeWebActivity extends WebActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loadTestData();
+        setUpTitleBar();
     }
 
-    @Override
-    protected boolean isNeedViewTitle() {
-        return false;
+    private void setUpTitleBar() {
+        getTitleBar().setTitle(getString(R.string.crude_oil_future));
     }
+
 
     private void loadTestData() {
         String testUrl = "https://www.dajiexin.com/";
