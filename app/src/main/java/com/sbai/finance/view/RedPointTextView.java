2e7fd65fc24b58b765f865640198bd5147339c94
licunkuan <licunkuan@hashxy.com>
2017-06-16 11:10:01 +0800

    消息条数超过1000条时显示红圈999+,不是自己时点击好心人条目跳转至用户详情,修改支付界面￥的大小

--- app/src/main/java/com/sbai/finance/view/RedPointTextView.java
+++ app/src/main/java/com/sbai/finance/view/RedPointTextView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 
@@ -34,13 +35,14 @@ public class RedPointTextView extends android.support.v7.widget.AppCompatTextVie
 	//圆心坐标
 	private int mX = 0;
 	private int mY = 0;
+	private int mNormalRadius;
 
 	public RedPointTextView(Context context) {
 		this(context, null);
 	}
 
 	public RedPointTextView(Context context, AttributeSet attrs) {
-		this(context, attrs,0);
+		this(context, attrs, 0);
 	}
 
 	public RedPointTextView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -64,12 +66,12 @@ public class RedPointTextView extends android.support.v7.widget.AppCompatTextVie
 		mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
 
 		typedArray.recycle();
+		mNormalRadius = dp2px(8);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-
 		mX = getWidth() * 29 / 50;
 		mY = getHeight() * 2 / 11;
 		mTextPaint.setTextSize(mTextSize);
@@ -77,20 +79,27 @@ public class RedPointTextView extends android.support.v7.widget.AppCompatTextVie
 		if (mNum > 0) {
 
 			mBgPaint.setColor(mBgColor);
-			canvas.drawCircle(mX, mY, dp2px(8), mBgPaint);
 
 			String str = null;
-			if (mNum > 99) {
-				str = "99+";
+			if (mNum > 999) {
+				str = "999+";
 			} else {
-				str = new String(mNum + "");
+				str = String.valueOf(mNum);
+			}
+			float textWidth = mTextPaint.measureText(str);
+			int padding = dp2px(2);
+			if (mNormalRadius * 2 - textWidth < padding) {
+				RectF rectF = new RectF(mX - textWidth / 2 - padding, mY - mNormalRadius, mX + textWidth / 2 + padding, mY + mNormalRadius);
+				canvas.drawRoundRect(rectF, mNormalRadius, mNormalRadius, mBgPaint);
+			} else {
+				canvas.drawCircle(mX, mY, mNormalRadius, mBgPaint);
 			}
 			//绘制的文本
 			mTextPaint.setColor(mTextColor);
-			canvas.drawText(str, mX - mTextPaint.measureText(str) / 2, mY + mTextPaint.getFontMetrics().bottom * 1.2f, mTextPaint);
+			canvas.drawText(str, mX - textWidth / 2, mY + mTextPaint.getFontMetrics().bottom * 1.2f, mTextPaint);
 		} else {
 			mBgPaint.setColor(0x00000000);
-			canvas.drawCircle(mX, mY, dp2px(8), mBgPaint);
+			canvas.drawCircle(mX, mY, mNormalRadius, mBgPaint);
 			String str = "";
 			//颜色
 			mTextPaint.setColor(0x00ffffff);
