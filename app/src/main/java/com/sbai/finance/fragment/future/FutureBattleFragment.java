683087dc9bb02cae9f2c1c731d81a087f6917e9d
林荣芳 <linrongfang@easybaocn.com>
2017-06-19 09:47:38 +0800

    增加自定义view和fragment

---
+++ app/src/main/java/com/sbai/finance/fragment/future/FutureBattleFragment.java
@@ -0,0 +1,11 @@
+package com.sbai.finance.fragment.future;
+
+import com.sbai.finance.fragment.BaseFragment;
+
+/**
+ * Created by linrongfang on 2017/6/19.
+ */
+
+public class FutureBattleFragment extends BaseFragment {
+
+}
---
+++ app/src/main/java/com/sbai/finance/view/BattleFloatView.java
@@ -0,0 +1,8 @@
+package com.sbai.finance.view;
+
+/**
+ * Created by linrongfang on 2017/6/19.
+ */
+
+public class BattleFloatView {
+}
