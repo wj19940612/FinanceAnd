5a8742086e5633b2aea98ae7d25f41cd6b091564
林荣芳 <linrongfang@easybaocn.com>
2017-06-20 14:55:33 +0800

    BattleButtons完成

---
+++ app/src/main/java/com/sbai/finance/view/stock/BattleTradeView.java
@@ -0,0 +1,31 @@
+package com.sbai.finance.view.stock;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by linrongfang on 2017/6/20.
+ */
+
+public class BattleTradeView extends LinearLayout {
+    public BattleTradeView(Context context) {
+        super(context);
+        init();
+    }
+
+    public BattleTradeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public BattleTradeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
