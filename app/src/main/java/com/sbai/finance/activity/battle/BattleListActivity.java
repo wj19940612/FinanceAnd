2773079d60a926793dfdddb024a4f49a3d714827
houcancan <houcancan@hashxy.com>
2017-07-03 15:41:53 +0800

    游戏列表请求最新数据调整到详情页

--- app/src/main/java/com/sbai/finance/activity/battle/BattleListActivity.java
+++ app/src/main/java/com/sbai/finance/activity/battle/BattleListActivity.java
@@ -259,9 +259,15 @@ public class BattleListActivity extends BaseActivity implements
                             item.setGameStatus(data.getGameStatus());
                             item.setEndTime(data.getEndTime());
                         }
+                        int pageType;
+                        if (data.getGameStatus()==Battle.GAME_STATUS_END){
+                            pageType = BattleActivity.PAGE_TYPE_RECORD;
+                        }else{
+                            pageType = BattleActivity.PAGE_TYPE_VERSUS;
+                        }
                         Launcher.with(getActivity(), BattleActivity.class)
                                 .putExtra(Launcher.EX_PAYLOAD, data)
-                                .putExtra(BattleActivity.PAGE_TYPE, BattleActivity.PAGE_TYPE_VERSUS)
+                                .putExtra(BattleActivity.PAGE_TYPE,pageType)
                                 .executeForResult(CANCEL_BATTLE);
 
                     }
