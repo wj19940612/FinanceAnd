f020a7bdb1c3215e0a661293dff4cc64b102a8f1
licunkuan <licunkuan@hashxy.com>
2017-08-09 09:45:21 +0800

    编写训练心得

--- app/src/main/java/com/sbai/finance/activity/trainexperience/TrainExperienceActivity.java
+++ app/src/main/java/com/sbai/finance/activity/trainexperience/TrainExperienceActivity.java
@@ -1,15 +1,42 @@
 package com.sbai.finance.activity.trainexperience;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.sbai.finance.R;
-
 public class TrainExperienceActivity extends AppCompatActivity {
+
+	/*@BindView(R.id.titleBar)
+	TitleBar mTitleBar;
+	@BindView(R.id.hotListView)
+	MyListView mHotListView;
+	@BindView(R.id.LatestListView)
+	MyListView mLatestListView;
+	@BindView(R.id.empty)
+	TextView mEmpty;
+	@BindView(R.id.scrollView)
+	ScrollView mScrollView;
+	@BindView(R.id.swipeRefreshLayout)
+	SwipeRefreshLayout mSwipeRefreshLayout;
+
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_train_experience);
+		ButterKnife.bind(this);
+
+		initHotExperienceList();
+		initLatestExperienceList();
+
+		requestLatestExperienceList();
+		initSwipeRefreshLayout();
 	}
+
+	private void initHotExperienceList() {
+		mHotExperienceListAdapter = new HotExperienceListAdapter(getActivity(), TAG);
+		mHotListView.setFocusable(false);
+		mHotListView.setAdapter(mHotExperienceListAdapter);
+	}
+
+	static class HotQuestionListAdapter extends ArrayAdapter<Question> {*/
 }
---
+++ app/src/main/java/com/sbai/finance/model/trainExperience/experience.java
@@ -0,0 +1,8 @@
+package com.sbai.finance.model.trainExperience;
+
+/**
+ * Created by lixiaokuan0819 on 2017/8/9.
+ */
+
+public class experience {
+}
