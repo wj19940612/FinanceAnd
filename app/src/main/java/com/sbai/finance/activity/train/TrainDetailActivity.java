ece079af7c205aed732ed86e402565c5cf53845f
licunkuan <licunkuan@hashxy.com>
2017-08-10 11:48:28 +0800

    修改姐说主页的消息图标

---
+++ app/src/main/java/com/sbai/finance/activity/train/TrainDetailActivity.java
@@ -0,0 +1,16 @@
+package com.sbai.finance.activity.train;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sbai.finance.R;
+
+
+public class TrainDetailActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_train_detail);
+	}
+}
