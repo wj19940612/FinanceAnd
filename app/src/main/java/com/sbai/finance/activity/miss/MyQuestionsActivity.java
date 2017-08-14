53b42acba2afd6de95c7436b0db3002e7e493c64
licunkuan <licunkuan@hashxy.com>
2017-08-14 14:10:08 +0800

    修改我的提问打赏后数据刷新

--- app/src/main/java/com/sbai/finance/activity/miss/MyQuestionsActivity.java
+++ app/src/main/java/com/sbai/finance/activity/miss/MyQuestionsActivity.java
@@ -77,7 +77,6 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 	private int mPageSize = 20;
 	private HashSet<Integer> mSet;
 	private View mFootView;
-	private RewardInfo mRewardInfo;
 	private List<Question> mMyQuestionList;
 	private RefreshReceiver mRefreshReceiver;
 	private MediaPlayerManager mMediaPlayerManager;
@@ -90,11 +89,10 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_questions);
 		ButterKnife.bind(this);
-		initRewardInfo();
 		mSet = new HashSet<>();
 		mMediaPlayerManager = new MediaPlayerManager(this);
 		mMyQuestionList = new ArrayList<>();
-		mMyQuestionAdapter = new MyQuestionAdapter(this, TAG);
+		mMyQuestionAdapter = new MyQuestionAdapter(this);
 		mMyQuestionAdapter.setOnClickCallback(new MyQuestionAdapter.OnClickCallback() {
 			@Override
 			public void onRewardClick(Question item) {
@@ -111,6 +109,15 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 					view.setBackgroundResource(R.drawable.ic_voice_4);
 					mPlayingPosition = -1;
 				} else {
+					//播放下一个之前把上一个播放位置的动画停了
+					if (mPlayingPosition != -1) {
+						View view1 = mListView.getChildAt(mPlayingPosition);
+						View voiceView = view1.findViewById(R.id.voiceLevel);
+						if (voiceView != null) {
+							voiceView.setBackgroundResource(R.drawable.ic_voice_4);
+						}
+					}
+
 					mAnimation.start();
 					if (!MissVoiceRecorder.isHeard(item.getId())) {
 						Client.listen(item.getId()).setTag(TAG).setCallback(new Callback<Resp<JsonPrimitive>>() {
@@ -167,28 +174,26 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 	@Override
 	protected void onPause() {
 		super.onPause();
+		//锁屏或者在后台运行或者跳转页面时停止播放和动画
 		mMediaPlayerManager.release();
+		if (mPlayingPosition != -1) {
+			View voiceView = mListView.getChildAt(mPlayingPosition).findViewById(R.id.voiceLevel);
+			if (voiceView != null) {
+				voiceView.setBackgroundResource(R.drawable.ic_voice_4);
+			}
+		}
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		mMediaPlayerManager.release();
-	}
-
-	private void initRewardInfo() {
-		mRewardInfo = new RewardInfo();
-		List<RewardMoney> list = new ArrayList<>();
-		RewardMoney rewardMoney = new RewardMoney();
-		rewardMoney.setMoney(10);
-		list.add(rewardMoney);
-		rewardMoney = new RewardMoney();
-		rewardMoney.setMoney(100);
-		list.add(rewardMoney);
-		rewardMoney = new RewardMoney();
-		rewardMoney.setMoney(1000);
-		list.add(rewardMoney);
-		mRewardInfo.setMoneyList(list);
+		if (mPlayingPosition != -1) {
+			View voiceView = mListView.getChildAt(mPlayingPosition).findViewById(R.id.voiceLevel);
+			if (voiceView != null) {
+				voiceView.setBackgroundResource(R.drawable.ic_voice_4);
+			}
+		}
 	}
 
 	private void initSwipeRefreshLayout() {
@@ -201,11 +206,6 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 			}
 		});
 	}
-
-	public RewardInfo getRewardInfo() {
-		return mRewardInfo;
-	}
-
 
 	private void requestMyQuestionList() {
 		Client.getMyQuestionList(mCreateTime, mPageSize).setTag(TAG)
@@ -299,7 +299,6 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 
 		private Context mContext;
 		private OnClickCallback mOnClickCallback;
-		private String TAG;
 
 		public void setOnClickCallback(OnClickCallback onClickCallback) {
 			mOnClickCallback = onClickCallback;
@@ -310,10 +309,9 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 			void onVoiceClick(Question item, int position, View view);
 		}
 
-		private MyQuestionAdapter(@NonNull Context context, String TAG) {
+		private MyQuestionAdapter(@NonNull Context context) {
 			super(context, 0);
 			this.mContext = context;
-			this.TAG = TAG;
 		}
 
 		@NonNull
@@ -329,7 +327,7 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 				viewHolder = (ViewHolder) convertView.getTag();
 			}
 
-			viewHolder.bindingData(mContext, getItem(position), mOnClickCallback, TAG);
+			viewHolder.bindingData(mContext, getItem(position), mOnClickCallback);
 			return convertView;
 		}
 
@@ -372,7 +370,7 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 			}
 
 			public void bindingData(final Context context, final Question item,
-			                        final OnClickCallback onClickCallback, final String TAG) {
+			                        final OnClickCallback onClickCallback) {
 				if (item == null) return;
 
 				Glide.with(context).load(item.getUserPortrait())
@@ -483,11 +481,13 @@ public class MyQuestionsActivity extends BaseActivity implements AdapterView.OnI
 		public void onReceive(Context context, Intent intent) {
 			if (ACTION_REWARD_SUCCESS.equalsIgnoreCase(intent.getAction())) {
 				if (intent.getIntExtra(Launcher.EX_PAYLOAD, -1) == RewardInfo.TYPE_QUESTION) {
-					for (Question question : mMyQuestionList) {
+					for (int i = 0; i < mMyQuestionAdapter.getCount(); i++ ) {
+						Question question = mMyQuestionAdapter.getItem(i);
 						if (question.getId() == intent.getIntExtra(Launcher.EX_PAYLOAD_1, -1)) {
 							int questionRewardCount = question.getAwardCount() + 1;
 							question.setAwardCount(questionRewardCount);
 							mMyQuestionAdapter.notifyDataSetChanged();
+							break;
 						}
 					}
 				}
