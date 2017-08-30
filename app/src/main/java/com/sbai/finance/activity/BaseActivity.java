c7ca40818d7ceeb69505f66c2eff4c052478fd22
John Zhang <zxukai@gmail.com>
2017-08-30 19:27:28 +0800

    修复 Android 6.0+ 版本的状态栏

--- app/src/main/java/com/sbai/finance/activity/BaseActivity.java
+++ app/src/main/java/com/sbai/finance/activity/BaseActivity.java
@@ -1,23 +1,17 @@
 package com.sbai.finance.activity;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.AbsListView;
 import android.widget.ScrollView;
 
@@ -42,7 +36,7 @@ import com.umeng.analytics.MobclickAgent;
 
 import java.security.NoSuchAlgorithmException;
 
-public class BaseActivity extends AppCompatActivity implements
+public class BaseActivity extends StatusBarActivity implements
         ApiIndeterminate, TimerHandler.TimerCallback {
 
     public static final String ACTION_TOKEN_EXPIRED = "com.sbai.fin.token_expired";
@@ -198,44 +192,6 @@ public class BaseActivity extends AppCompatActivity implements
         });
     }
 
-    protected void translucentStatusBar() {
-        //make full transparent statusBar
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
-        }
-        if (Build.VERSION.SDK_INT >= 19) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
-    }
-
-    private void setWindowFlag(Activity activity, final int bits, boolean on) {
-        Window win = activity.getWindow();
-        WindowManager.LayoutParams winParams = win.getAttributes();
-        if (on) {
-            winParams.flags |= bits;
-        } else {
-            winParams.flags &= ~bits;
-        }
-        win.setAttributes(winParams);
-    }
-
-    protected void addStatusBarHeightTopPadding(View view) {
-        int paddingTop = getStatusBarHeight();
-        view.setPadding(0, paddingTop, 0, 0);
-    }
-
-    private int getStatusBarHeight() {
-        int result = 0;
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
-    }
 
     /**
      * 友盟统计埋点
