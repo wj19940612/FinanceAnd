8976347278325d1c96dabc8c3637048f5e53fc47
John Zhang <zxukai@gmail.com>
2017-09-04 11:58:43 +0800

    闪屏主题修改

--- app/src/main/java/com/sbai/finance/activity/SplashActivity.java
+++ app/src/main/java/com/sbai/finance/activity/SplashActivity.java
@@ -37,7 +37,7 @@ public class SplashActivity extends BaseActivity {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(1500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
