98303f84ce4eaeca668942f4ebbe8cd44101bbfb
John Zhang <zxukai@gmail.com>
2017-09-12 19:00:35 +0800

    delete glide custom config

--- glide/src/main/java/com/sbai/glide/MyAppGlideModule.java
+++ glide/src/main/java/com/sbai/glide/MyAppGlideModule.java
@@ -1,11 +1,6 @@
 package com.sbai.glide;
 
-import android.content.Context;
-
-import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.load.engine.cache.ExternalCacheDiskCacheFactory;
-import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
 import com.bumptech.glide.module.AppGlideModule;
 
 @GlideModule
@@ -13,14 +8,5 @@ public final class MyAppGlideModule extends AppGlideModule {
     @Override
     public boolean isManifestParsingEnabled() {
         return false;
-    }
-
-    @Override
-    public void applyOptions(Context context, GlideBuilder builder) {
-        MemorySizeCalculator calculator = new MemorySizeCalculator.Builder(context)
-                .setMemoryCacheScreens(2)
-                .build();
-        builder.setMemorySizeCalculator(calculator);
-        builder.setDiskCache(new ExternalCacheDiskCacheFactory(context));
     }
 }
