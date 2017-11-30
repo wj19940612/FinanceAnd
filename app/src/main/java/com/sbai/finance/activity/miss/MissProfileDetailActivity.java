0a7c958032f6501eecc8e2042ae61cdad9194588
zhangjianfeng <zhangjianfeng@hashxy.com>
2017-11-30 15:44:12 +0800

    小姐姐主页刷新按钮显示bug

--- app/src/main/java/com/sbai/finance/activity/miss/MissProfileDetailActivity.java
+++ app/src/main/java/com/sbai/finance/activity/miss/MissProfileDetailActivity.java
@@ -293,8 +293,8 @@ public class MissProfileDetailActivity extends BaseActivity implements MissProfi
                 .setCallback(new Callback2D<Resp<Miss>, Miss>() {
                     @Override
                     protected void onRespSuccessData(Miss miss) {
-                        updateMissDetail(miss);
                         setMiss(miss);
+                        updateMissDetail(miss);
                     }
 
                     @Override
@@ -345,7 +345,7 @@ public class MissProfileDetailActivity extends BaseActivity implements MissProfi
     }
 
     private void updateNullMissDetail(){
-        setPositionBtn(0);
+        setPositionBtn(mPosition);
         GlideApp.with(this).load(null)
                 .placeholder(R.drawable.ic_default_avatar)
                 .circleCrop()
