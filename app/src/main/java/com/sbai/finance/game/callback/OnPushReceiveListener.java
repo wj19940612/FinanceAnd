6ae82e1908dc7dd1b26669ffc0cdf7bd359df0fb
John Zhang <zxukai@gmail.com>
2017-12-20 17:08:23 +0800

    remove useless code

--- app/src/main/java/com/sbai/finance/game/callback/OnPushReceiveListener.java
+++ app/src/main/java/com/sbai/finance/game/callback/OnPushReceiveListener.java
@@ -1,7 +1,5 @@
 package com.sbai.finance.game.callback;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.internal.$Gson$Types;
 
 import java.lang.reflect.ParameterizedType;
@@ -10,15 +8,6 @@ import java.lang.reflect.Type;
 public abstract class OnPushReceiveListener<T> {
 
     public abstract void onPushReceive(T t, String originalData);
-
-    public void onOriginPushReceive(Object o, String originalData) {
-        try {
-            T t = new Gson().fromJson(new Gson().toJson(o), getGenericType());
-            onPushReceive(t, originalData);
-        } catch (JsonSyntaxException e) {
-            e.printStackTrace();
-        }
-    }
 
     public Type getGenericType() {
         Type superclass = getClass().getGenericSuperclass();
