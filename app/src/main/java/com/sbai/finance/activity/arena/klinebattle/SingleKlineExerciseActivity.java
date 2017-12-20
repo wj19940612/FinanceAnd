49a07b19ff658ded30a0631b991238944c2269a5
houcancan <houcancan@hashxy.com>
2017-12-20 18:16:16 +0800

    修改单人训练页

--- app/src/main/java/com/sbai/finance/activity/arena/klinebattle/SingleKlineExerciseActivity.java
+++ app/src/main/java/com/sbai/finance/activity/arena/klinebattle/SingleKlineExerciseActivity.java
@@ -147,11 +147,6 @@ public class SingleKlineExerciseActivity extends BaseActivity {
                 }).fireFree();
     }
 
-    @OnClick(R.id.back)
-    public void onViewClicked() {
-        getActivity().onBackPressed();
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
