bc070e5855734b13a0eb33792f26b7308ea1090c
John Zhang <zxukai@gmail.com>
2017-12-26 23:21:21 +0800

    修改持仓背景画图算法

--- app/src/main/java/com/sbai/finance/view/klinebattle/BattleKlineChart.java
+++ app/src/main/java/com/sbai/finance/view/klinebattle/BattleKlineChart.java
@@ -26,6 +26,7 @@ public class BattleKlineChart extends KlineChart {
     private static final String TRANS_GREEN = "#262fcc9f";
     private static final String TRANS_RED = "#26f25b57";
     private List<BattleKlineData> mDataList;
+    private double mBuyPrice;
 
     protected void setTranslucentBgPaint(Paint paint, String color) {
         paint.setColor(Color.parseColor(color));
@@ -82,75 +83,52 @@ public class BattleKlineChart extends KlineChart {
     @Override
     protected void drawRealTimeData(boolean indexesEnable, int left, int top, int width, int height, int left2, int top2, int width2, int height2, Canvas canvas) {
         super.drawRealTimeData(indexesEnable, left, top, width, height, left2, top2, width2, height2, canvas);
+
         // draw translucent background
-        if (getDataList() == null || getDataList().isEmpty()) return;
-        /*
-         * 透明背景绘制区：
-         * buy 点 ~ sell 点
-         * buy 点 ~ (end - 1) & Y 点
-         *
-         * (start & HP) 点 ~ sell 点
-         * (start & HP) 点 ~ (end - 1) & Y 点
-         *
-         * (start & HP ) 点 ~ (end - 1 & HP) 点
-         * buy 点 ~ ~ (end - 1 & HP) 点
-         *
-         * (end - 1 & HP) 点的颜色需要遍历到 sell 点 或者最后一个 Y 点才能确定
-         * 并且当这个点已经是最后一个点的时候，需要看 lastInvisibleData 的持仓才能确定
-         */
+        List<KlineViewData> dataList = getDataList();
+        if (dataList == null || dataList.isEmpty()) return;
         RectF rectF = null;
-        int i = getStart();
-        for (; i < getEnd(); i++) {
+        for (int i = 0; i < dataList.size(); i++) {
             BattleKlineData data = (BattleKlineData) getDataList().get(i);
-            if (rectF != null && data.getMark().equalsIgnoreCase(BattleKlineData.MARK_SELL)) {
-                rectF.right = getChartXOfScreen(i);
-                String bgColor = data.getPositions() >= 0 ? TRANS_RED : TRANS_GREEN;
-                setTranslucentBgPaint(sPaint, bgColor);
-                canvas.drawRect(rectF, sPaint);
-                rectF = null;
-            } else if (rectF != null && i == getEnd() - 1 && data.getMark().equalsIgnoreCase(BattleKlineData.MARK_OP)) {
-                rectF.right = getChartXOfScreen(i);
-                String bgColor = data.getPositions() >= 0 ? TRANS_RED : TRANS_GREEN;
-                setTranslucentBgPaint(sPaint, bgColor);
-                canvas.drawRect(rectF, sPaint);
-                rectF = null;
-            } else if (rectF != null && i == getEnd() - 1 && data.getMark().equalsIgnoreCase(BattleKlineData.MARK_NOT_OP)) { // 最后一个结束数据
-                rectF.right = getChartXOfScreen(i);
-                String bgColor = data.getPositions() >= 0 ? TRANS_RED : TRANS_GREEN;
-                setTranslucentBgPaint(sPaint, bgColor);
-                canvas.drawRect(rectF, sPaint);
-                rectF = null;
-            } else if (i == getStart() && data.getMark().equalsIgnoreCase(BattleKlineData.MARK_HOLD_PASS)) {
-                rectF = getRectF();
-                rectF.top = top;
-                rectF.bottom = top + height;
-                rectF.left = left;
-                rectF.right = left + width - mPriceAreaWidth;
-            } else if (data.getMark().equalsIgnoreCase(BattleKlineData.MARK_BUY)) {
-                rectF = getRectF();
-                rectF.top = top;
-                rectF.bottom = top + height;
-                rectF.left = getChartXOfScreen(i);
-                rectF.right = left + width - mPriceAreaWidth;
-            }
-        } // i == end
-        if (rectF != null) {
-            String bgColor = TRANS_RED;
-            for (; i < getDataList().size(); i++) {
-                BattleKlineData data = (BattleKlineData) getDataList().get(i);
-                if (data.getMark().equalsIgnoreCase(BattleKlineData.MARK_SELL)) {
-                    bgColor = data.getPositions() >= 0 ? TRANS_RED : TRANS_GREEN;
-                    break;
-                } else if (data.getMark().equalsIgnoreCase(BattleKlineData.MARK_OP)) {
-                    bgColor = data.getPositions() >= 0 ? TRANS_RED : TRANS_GREEN;
-                    break;
-                } else if (data.getMark().equalsIgnoreCase(BattleKlineData.MARK_NOT_OP)) { // 最后一个结束数据
-                    bgColor = data.getPositions() >= 0 ? TRANS_RED : TRANS_GREEN;
-                    break;
+            if (data.getMark().equalsIgnoreCase(BattleKlineData.MARK_BUY)) { // buy point
+                mBuyPrice = data.getClosePrice();
+
+                if (i < getStart()) { // 屏幕外，左边
+                    rectF = getRectF();
+                    rectF.left = left;
+                } else if (i < getEnd()) { // 屏幕内
+                    rectF = getRectF();
+                    rectF.left = getChartXOfScreen(i);
                 }
+
+                if (rectF != null) {
+                    rectF.top = top;
+                    rectF.bottom = top + height;
+                    rectF.right = left + width - mPriceAreaWidth;
+                }
+
+            } else if (rectF != null && data.getMark().equalsIgnoreCase(BattleKlineData.MARK_SELL)) { // sell point
+                if (i < getStart()) { // 屏幕外，左边, 不画
+                    rectF = null;
+                } else if (i < getEnd()) { // 屏幕内，更新 right
+                    rectF.right = getChartXOfScreen(i);
+                }
+
+                if (rectF != null) {
+                    String bgColor = data.getClosePrice() - mBuyPrice >= 0 ? TRANS_RED : TRANS_GREEN;
+                    setTranslucentBgPaint(sPaint, bgColor);
+                    canvas.drawRect(rectF, sPaint);
+                    rectF = null;
+                }
+            } else if (rectF != null && i == dataList.size() - 1) { // 最后的数据，包括 Y，N
+                if (i >= getStart() && i < getEnd()) {
+                    rectF.right = getChartXOfScreen(i);
+                }
+                String bgColor = data.getClosePrice() - mBuyPrice >= 0 ? TRANS_RED : TRANS_GREEN;
+                setTranslucentBgPaint(sPaint, bgColor);
+                canvas.drawRect(rectF, sPaint);
+                rectF = null;
             }
-            setTranslucentBgPaint(sPaint, bgColor);
-            canvas.drawRect(rectF, sPaint);
         }
     }
 
@@ -194,6 +172,7 @@ public class BattleKlineChart extends KlineChart {
                     start = Math.max(start, mv);
                 }
                 start = start - 1;
+                kline.clearData();
                 kline.setDataList(new ArrayList<KlineViewData>(mDataList.subList(start, mDataList.size())));
             }
         }
